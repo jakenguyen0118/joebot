37657eb37c26b8719f13a326568796c4a1480e42
Jake Nguyen <jakey@Jakes-Air.fios-router.home>
2020-11-10 13:43:32 -0500

    partytime command

--- bot/bot.py
+++ bot/bot.py
@@ -119,6 +119,39 @@ async def greece(ctx):
     response = random.choice(greece_pics)
     msg = love_greece + response
     await ctx.send(msg)
-    
+
+
+@bot.command(name='partytime', help='Recommendation for drinks')
+async def partytime(ctx):
+    drinks = [
+        'Chase\'s preferred brand of Pickle Beer - https://untappd.com/b/martin-house-brewing-company-best-maid-sour-pickle-beer/2646998',
+        'Lawson\'s Finest Liquids\' Little Sip - https://oct.co/essays/lawsons-little-sip-review',
+        'Tree House\'s Juice Machine - https://oct.co/essays/tree-house-juice-machine-review',
+        'Other Half\'s All Together IPA - https://oct.co/essays/other-half-all-together-ipa-review',
+        'New England Brewing Company\'s Fuzzy Baby Ducks - https://oct.co/essays/new-england-fuzzy-baby-ducks-review',
+        'Abomination\'s Wandering Into the Fog Mosaic - https://oct.co/essays/wandering-fog-mosaic-abomination-review',
+        'O\'so\'s Hop Debacle - https://oct.co/essays/oso-hop-debacle-review',
+        'Half Acre\'s Double Daisy Cutter - https://oct.co/essays/half-acre-double-daisy-cutter-review',
+        'Omnipollo\'s Lustro - https://oct.co/essays/omnipollo-lustro-review',
+        'Equilibrium\'s Energy Equals - https://oct.co/essays/equilbrium-energy-equals-review',
+        'Short Throw\'s Code of the Streets - https://oct.co/essays/short-throw-code-streets-review',
+        'American Solera\'s Terpy Galaxy - https://oct.co/essays/american-solera-terpy-galaxy-review',
+        'Offshoot\'s Retreat - https://oct.co/essays/offshoot-retreat-review',
+        'Talea\'s Mangotango - https://oct.co/essays/talea-mangotango-review',
+        'Phase Three\'s Crème - https://oct.co/essays/phase-three-creme-review',
+        'WeldWorks\' Juicy Bits - https://oct.co/essays/weldwerks-juicy-bits-review',
+        'Nod Hill\'s Mood for a Day - https://oct.co/essays/hazy-ipa-mood',
+        'Hi-Wire\'s Hi-Pitch Mosaic IPA - https://oct.co/essays/hi-wire-hi-pitch-mosaic-ipa-review',
+        'Elysian\'s Dayglow - https://oct.co/essays/elysian-dayglow-ipa-review',
+        'New Belgium\'s Voodoo Ranger 1985 IPA - https://oct.co/essays/new-belgium-voodoo-ranger-1985-review',
+        'Fat Orange Cat x Nightmare Brewing\'s Feed Me a Stray Cat - https://oct.co/essays/fat-orange-cat-nightmare-feed-me-stray-cat-review',
+        'Bell\'s Light Hearted Ale - https://oct.co/essays/bells-light-hearted-review'
+    ]
+
+    msg = 'I would recommend '
+    drink = random.choice(drinks)
+    response = msg + drink
+    await ctx.send(response)
+
 
 bot.run(TOKEN)
